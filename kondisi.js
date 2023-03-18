c7a95c0c7e91fcc47c650502045d2171c0072246
dwirustanto10 <keizaq100282@gmail.com>
2023-03-18 10:54:23 +0700

    pertemuan kedua

---
+++ kondisi.js
@@ -0,0 +1,48 @@
+// var angka = 1;
+// if (angka == 2) {
+//   console.log('angka 1');
+// } else {
+//   console.log('angka selain 1');
+// }
+
+// var angka = 1;
+// if (angka == 2 || angka == 1) {
+//   console.log('angka 1');
+// } else {
+//   console.log('angka selain 1');
+// }
+
+// kondisi 2
+// var umur = 17;
+// if (umur >= 17) {
+//   console.log('Bisa bikin KTP');
+// } else {
+//   console.log('Belum bisa bikin KTP');
+// }
+
+// var minum = 'susu';
+// switch (minum) {
+//   case 'kopi':
+//     console.log('anda pilih kopi');
+//     break;
+//   case 'teh':
+//     console.log('anda pilih teh');
+//     break;
+//   default:
+//     console.log('tidak haus');
+// }
+
+var umur = 16;
+var tinggi = 150;
+
+if (umur < 17) {
+  if (tinggi < 150) {
+    console.log('Tidak boleh masuk');
+  } else if ((tinggi = 150)) {
+    console.log('Boleh masuk');
+  } else {
+    console.log('Tidak Boleh');
+  }
+} else {
+  console.log('boleh masuk');
+}
