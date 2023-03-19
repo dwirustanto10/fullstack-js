d9dd76f82e8d5f8408b5e97f3180623f03a06b88
dwirustanto10 <keizaq100282@gmail.com>
2023-03-19 15:34:02 +0700

    fungsi

--- fungsi.js
+++ fungsi.js
@@ -13,12 +13,70 @@
 // hello('Selaamat pagi peserta');
 
 // mencetak bilangan ganjil apa genap
-function bilangan(g) {
-  if (g % 2 === 0) {
-    console.log('Hari ini berlaku/khusus untuk plat no Genap');
-  } else {
-    console.log('Hari ini berlaku/khusus untuk plat no Ganjil');
-  }
+// function bilangan(g) {
+//   if (g % 2 === 0) {
+//     console.log('Hari ini berlaku/khusus untuk plat no Genap');
+//   } else {
+//     console.log('Hari ini berlaku/khusus untuk plat no Ganjil');
+//   }
+// }
+
+// bilangan(1304);
+
+// function splitToArray(stringnya) {
+//   var temparray = stringnya.split(' ');
+//   return temparray;
+// }
+// function jmlKata(kalimatnya) {
+//   var hasilspilt = splitToArray(kalimatnya);
+//   var arrayLength = hasilspilt.length;
+//   return arrayLength;
+// }
+// console.log(jmlKata('selamat siang kawan ku semua'));
+
+function kali(a, b) {
+  return a * b;
 }
 
-bilangan(1304);
+function bagi(a, b) {
+  return a / b;
+}
+
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+function modulus(a, b) {
+  return a % b;
+}
+function calculatorUtama(a, b, operasi) {
+  var hasil;
+  switch (operasi) {
+    case 'kali':
+      hasil = kali(a, b);
+      break;
+    case 'bagi':
+      hasil = bagi(a, b);
+      break;
+    case 'tambah':
+      hasil = tambah(a, b);
+      break;
+    case 'kurang':
+      hasil = kurang(a, b);
+      break;
+    case 'modulus':
+      hasil = modulus(a, b);
+      break;
+
+    default:
+      hasil = 0;
+  }
+  return hasil;
+}
+
+var result = calculatorUtama(3, 4, 'kali');
+
+console.log(result);
