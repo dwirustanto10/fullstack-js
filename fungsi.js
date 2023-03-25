f367552a25c83b18772c0d3136b642485987ec12
dwirustanto10 <keizaq100282@gmail.com>
2023-03-25 08:44:20 +0700

    ok

--- fungsi.js
+++ fungsi.js
@@ -33,86 +33,86 @@
 //   return arrayLength;
 // }
 // console.log(jmlKata('selamat siang kawan ku semua'));
-// var memory = [0];
+var memory = [0];
 
-// function kali(a, b) {
-//   return a * b;
-// }
-
-// function bagi(a, b) {
-//   return a / b;
-// }
-
-// function tambah(a, b) {
-//   return a + b;
-// }
-
-// function kurang(a, b) {
-//   return a - b;
-// }
-// function modulus(a, b) {
-//   return a % b;
-// }
-// function calculatorUtama(a, b, operasi) {
-//   var hasil;
-//   switch (operasi) {
-//     case '*':
-//       hasil = kali(a, b);
-//       break;
-//     case '/':
-//       hasil = bagi(a, b);
-//       break;
-//     case '+':
-//       hasil = tambah(a, b);
-//       break;
-//     case '-':
-//       hasil = kurang(a, b);
-//       break;
-//     case '%':
-//       hasil = modulus(a, b);
-//       break;
-
-//     default:
-//       hasil = 0;
-//   }
-//   return hasil;
-// }
-
-// // var result = calculatorUtama(4, 4, 'bagi');
-// var result = calculatorUtama(8, 4, '%');
-// memory.push(result);
-
-// console.log(result);
-// console.log(memory);
-
-function segitiga(a, t) {
-  return a * t * 0.5;
-}
-function persegi(a, t) {
-  return a * t;
-}
-function lingkaran(a, t) {
-  return a * t * 3.14;
+function kali(a, b) {
+  return a * b;
 }
 
-function luasBangun(a, t, bangun) {
+function bagi(a, b) {
+  return a / b;
+}
+
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+function modulus(a, b) {
+  return a % b;
+}
+function calculatorUtama(a, b, operasi) {
   var hasil;
-  switch (bangun) {
-    case 'segi':
-      hasil = segitiga(a, t);
+  switch (operasi) {
+    case '*':
+      hasil = kali(a, b);
       break;
-    case 'kotak':
-      hasil = persegi(a, t);
+    case '/':
+      hasil = bagi(a, b);
       break;
-    case 'bulat':
-      hasil = lingkaran(a, t);
+    case '+':
+      hasil = tambah(a, b);
       break;
+    case '-':
+      hasil = kurang(a, b);
+      break;
+    case '%':
+      hasil = modulus(a, b);
+      break;
+
     default:
-      hasil = 'belum ada';
-      break;
+      hasil = 0;
   }
   return hasil;
 }
 
-var hasilLuas = luasBangun(2, 2, 'bulat');
-console.log(hasilLuas);
+// var result = calculatorUtama(4, 4, 'bagi');
+var result = calculatorUtama(9, 4, '%');
+memory.push(result);
+
+console.log(result);
+console.log(memory);
+
+// function segitiga(a, t) {
+//   return a * t * 0.5;
+// }
+// function persegi(a, t) {
+//   return a * t;
+// }
+// function lingkaran(a, t) {
+//   return a * t * 3.14;
+// }
+
+// function luasBangun(a, t, bangun) {
+//   var hasil;
+//   switch (bangun) {
+//     case 'segi':
+//       hasil = segitiga(a, t);
+//       break;
+//     case 'kotak':
+//       hasil = persegi(a, t);
+//       break;
+//     case 'bulat':
+//       hasil = lingkaran(a, t);
+//       break;
+//     default:
+//       hasil = 'belum ada';
+//       break;
+//   }
+//   return hasil;
+// }
+
+// var hasilLuas = luasBangun(2, 2, 'bulat');
+// console.log(hasilLuas);
